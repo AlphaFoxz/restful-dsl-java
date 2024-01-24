6b0740cb76f0172c2cf6a0b7670df8d9a550e48f
EricFox <841958335@qq.com>
2024-01-24 08:45:19 +0800

    测试多模块2

--- starter/build.gradle.kts
+++ starter/build.gradle.kts
@@ -2,10 +2,10 @@ var restulDslGroup = "com.github.alphafoxz.restful-dsl-java"
 var restulDslVersion = "3.0.0-alpha.0"
 tasks.bootJar {
     enabled = false
-//        archiveClassifier.set("")
 }
 tasks.jar {
     enabled = true
+    archiveClassifier.set("")
 }
 dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter-web")
