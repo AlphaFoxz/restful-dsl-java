a3387032fb130857bc08ae5e505222d21ca913e4
EricFox <841958335@qq.com>
2024-01-24 09:14:08 +0800

    fix: bean重复的问题

--- test/src/main/java/com/github/alphafoxz/restful_dsl/test/configuration/SnowFlakeConfiguration.java
+++ test/src/main/java/com/github/alphafoxz/restful_dsl/test/configuration/SnowFlakeConfiguration.java
@@ -1,14 +1,12 @@
-package com.github.alphafoxz.restful_dsl.starter.configuration;
+package com.github.alphafoxz.restful_dsl.test.configuration;
 
 import cn.hutool.core.lang.Snowflake;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RestfulDslConfiguration {
+public class SnowFlakeConfiguration {
     @Bean
-    @ConditionalOnMissingBean(Snowflake.class)
     public Snowflake snowflake() {
         return new Snowflake(0, 1);
     }
