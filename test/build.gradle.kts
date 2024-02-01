0d400f4ba3f15c218f5a8917b1e85b6efc5bbf97
EricFox <841958335@qq.com>
2024-02-01 15:46:21 +0800

    fix

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -4,10 +4,7 @@ tasks.bootJar {
     enabled = true
 }
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web") {
-        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
-    }
-    implementation("org.springframework.boot:spring-boot-starter-undertow")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
