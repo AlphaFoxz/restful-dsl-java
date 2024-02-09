9eb4fb02d2986f95295f5e44743527451d966d01
AlphaFoxz <841958335@qq.com>
2024-02-09 12:51:19 +0800

    test

--- test/src/main/java/com/github/alphafoxz/restful_dsl/test/configuration/SpringDocConfiguration.java
+++ test/src/main/java/com/github/alphafoxz/restful_dsl/test/configuration/SpringDocConfiguration.java
@@ -1,54 +1,53 @@
 package com.github.alphafoxz.restful_dsl.test.configuration;
 
-import cn.hutool.core.collection.CollUtil;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.StringVendorExtension;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-@EnableOpenApi
 @Configuration
 public class SpringDocConfiguration {
     @Bean
-    public Docket restfulApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .groupName("restful_dsl-starter")
-                .apiInfo(getInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.github.alphafoxz.restful_dsl.starter"))
-//                .paths(PathSelectors.regex("/_restfulDsl/*+"))
+    public GroupedOpenApi restfulApi() {
+        return GroupedOpenApi.builder()
+                .group("restful_dsl-starter")
+                .packagesToScan("com.github.alphafoxz.restful_dsl.starter")
+//                .pathsToMatch("/_restfulDsl/**")
                 .build();
     }
 
     @Bean
-    public Docket testApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .groupName("restful_dsl-test")
-                .apiInfo(getInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.github.alphafoxz.restful_dsl.test"))
-//                .paths(PathSelectors.regex("/test/*"))
+    public GroupedOpenApi testApi() {
+        return GroupedOpenApi.builder()
+                .group("restful_dsl-test")
+                .packagesToScan("com.github.alphafoxz.restful_dsl.test")
                 .build();
     }
 
-    private ApiInfo getInfo() {
-        ApiInfoBuilder builder = new ApiInfoBuilder().title("RESTful DSL 测试 API文档")
+    @Bean
+    public OpenAPI indexApi() {
+        return new OpenAPI()
+                .info(getInfo())
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringDoc Documentation")
+                        .url("https://springdoc.org/"));
+    }
+
+    private Info getInfo() {
+        Info info = new Info().title("RESTful DSL 测试 API文档")
                 .description("基于SpringDoc生成的API文档")
-                .version("0.0.1-alpha.0")
-                .licenseUrl("https://github.com/AlphaFoxz/restful-dsl-java/blob/main/LICENSE");
-        Contact contact = new Contact(
-                "AlphaFoxz",
-                "https://github.com/AlphaFoxz",
-                "841958335@qq.com"
-        );
-        builder.contact(contact);
-        builder.extensions(CollUtil.newArrayList(new StringVendorExtension("项目地址（java）", "https://github.com/AlphaFoxz/restful-dsl-java")));
-        return builder.build();
+                .version("0.1.0-alpha.0")
+                .license(new License().name("Apache 2.0").url("https://github.com/AlphaFoxz/restful-dsl-java/blob/main/LICENSE"));
+        info.addExtension("sdk项目地址（java）", "https://github.com/AlphaFoxz/restful-dsl-java");
+        Contact contact = new Contact();
+        contact.setName("AlphaFoxz");
+        contact.setEmail("841958335@qq.com");
+        contact.setUrl("https://github.com/AlphaFoxz");
+        info.contact(contact);
+        return info;
     }
 }
